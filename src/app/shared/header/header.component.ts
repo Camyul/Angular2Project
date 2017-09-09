d7b93ed692327bcf0be92e669f940ff9aa13a6ef
Emil Dimitrov <emil_132@abv.bg>
2017-09-09 16:28:54 +0300

    Added user avatar photo on header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -9,10 +9,12 @@ import { AuthService } from '../../providers/auth.service';
 export class HeaderComponent implements OnInit {
 
   public user;
+  public userPhoto;
   public isLoggedIn: Boolean = false;
 
   constructor(private authService: AuthService) {
     this.user = this.authService.user;
+    this.userPhoto = this.authService.af.auth.currentUser.photoURL;
     this.authService.af.authState.subscribe(
       (auth) => {
         if (auth == null) {
