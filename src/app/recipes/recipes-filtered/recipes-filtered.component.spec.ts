def0596c57ba5aee197cbf9301136cf5c995b8c9
Emil Dimitrov <emil_132@abv.bg>
2017-09-10 15:34:05 +0300

    Created Filter by ingredient component

---
+++ src/app/recipes/recipes-filtered/recipes-filtered.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipesFilteredComponent } from './recipes-filtered.component';
+
+describe('RecipesListComponent', () => {
+  let component: RecipesFilteredComponent;
+  let fixture: ComponentFixture<RecipesFilteredComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RecipesFilteredComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecipesFilteredComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
