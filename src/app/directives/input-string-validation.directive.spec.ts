71ee4d662ee50936506d2b81e3983608b7a37e21
Emil Dimitrov <emil_132@abv.bg>
2017-09-11 07:54:18 +0300

    Remove unneeded spec files

--- src/app/directives/input-string-validation.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InputStringValidationDirective } from './input-string-validation.directive';
-
-/*describe('InputStringValidationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InputStringValidationDirective();
-    expect(directive).toBeTruthy();
-  });
-});*/
--- src/app/directives/input-year-validation.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InputYearValidationDirective } from './input-year-validation.directive';
-
-/*describe('InputYearValidationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InputYearValidationDirective();
-    expect(directive).toBeTruthy();
-  });
-});*/
